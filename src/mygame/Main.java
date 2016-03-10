651d780ce45ee0d7e19f80a941e3e846c1786f78
mpearce25 <pearcematthew25@gmail.com>
2016-03-10 08:37:06 -0500

    added things - rotates around point

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -8,8 +8,9 @@ import com.jme3.scene.*;
 public class Main extends SimpleApplication {
  
     Node bottomRow = new Node();
+     Node testRow = new Node();
     //Quaternion rotation=(new Quaternion()).fromAngles(145 ,78, 19);
-    Quaternion rotation=(new Quaternion()).fromAngles(0, 0, 0);
+    Quaternion rotation=(new Quaternion()).fromAngles(0, 0,0);
 Quaternion rotationVelocity=(new Quaternion()).fromAngles(0.0f,1.0f, 0.0f);
 
 Spatial cube9;
@@ -40,20 +41,34 @@ Spatial cube9;
         Spatial cube3 = createSpatial("Models/C-rwg/C-rwg.j3o", new Vector3f(-4,0,0), 180,90,0);
         rootNode.attachChild(cube3);
         
-        Spatial cube7 = createSpatial("Models/C-ryb/C-ryb.j3o", new Vector3f(0,-4,0), 180,0,0);
+        Spatial cube4 = createSpatial("Models/E-rb/E-rb.j3o", new Vector3f(0,-2,0), 270,90,0);
+        rootNode.attachChild(cube4);
+        
+        Spatial cube5 = createSpatial("Models/S-r/S-r.j3o", new Vector3f(-2,-2,0), 180,90,0);
+        rootNode.attachChild(cube5);
+        
+        Spatial cube6 = createSpatial("Models/E-rg/E-rg.j3o", new Vector3f(-4,-2,0), 180,90,0);
+        rootNode.attachChild(cube6);
+        
+        
+        
+        Spatial cube7 = createSpatial("Models/C-ryb/C-ryb.j3o", new Vector3f(2,0,0), 180,0,0);
         bottomRow.attachChild(cube7);
         
         
         rootNode.attachChild(bottomRow);
-        Spatial cube8 = createSpatial("Models/E-ry/E-ry.j3o", new Vector3f(-2,-4,0), 0,270,180);
+        Spatial cube8 = createSpatial("Models/E-ry/E-ry.j3o", new Vector3f(0,0,0), 0,270,180);
         //rootNode.attachChild(cube8);
         bottomRow.attachChild(cube8);
         
-        cube9 = createSpatial("Models/C-ryg/C-ryg.j3o", new Vector3f(-4,-4,0), 180,270,0);
+        cube9 = createSpatial("Models/C-ryg/C-ryg.j3o", new Vector3f(-2,0,0), 180,270,0);
         //rootNode.attachChild(cube9);
         bottomRow.attachChild(cube9);
         
+        bottomRow.setLocalTranslation(new Vector3f(-2,-4, 0));
+        
        // rootNode.attachChild();
+     
      
         
     }
@@ -81,7 +96,11 @@ public void simpleUpdate(float tpf) {
     rotationVelocityFPS.slerp(Quaternion.IDENTITY, rotationVelocity, tpf);
 
     rotation.multLocal(rotationVelocityFPS);
+    
     bottomRow.setLocalRotation(rotation);
+    //bottomRow.rotate(FastMath.DEG_TO_RAD * 180f, 0f, 0f);
+    //testRow.rotate(rotation);
+    
 }
     
     public Spatial createSpatial(String modelPath, Vector3f translation, float rotX, float rotY, float rotZ ){
