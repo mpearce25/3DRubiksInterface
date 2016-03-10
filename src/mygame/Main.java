b04a73465de0a7696a5023e746dc432c8b0c75d2
mpearce25 <pearcematthew25@gmail.com>
2016-03-09 23:46:42 -0500

    more layers + animation

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -7,6 +7,11 @@ import com.jme3.scene.*;
 
 public class Main extends SimpleApplication {
  
+    
+    Quaternion rotation=(new Quaternion()).fromAngles(0,0, 0);
+Quaternion rotationVelocity=(new Quaternion()).fromAngles(0.25f,0.5f, 0.25f);
+
+Spatial cube9;
     public static void main(String[] args){
         Main app = new Main();
         app.start();
@@ -32,7 +37,17 @@ public class Main extends SimpleApplication {
         
         Spatial cube3 = createSpatial("Models/C-rwg/C-rwg.j3o", new Vector3f(-4,0,0), 180,90,0);
         rootNode.attachChild(cube3);
+        
+        Spatial cube7 = createSpatial("Models/C-ryb/C-ryb.j3o", new Vector3f(0,-4,0), 180,0,0);
+        rootNode.attachChild(cube7);
+        
+        Spatial cube8 = createSpatial("Models/E-ry/E-ry.j3o", new Vector3f(-2,-4,0), 0,270,180);
+        rootNode.attachChild(cube8);
+        
+        cube9 = createSpatial("Models/C-ryg/C-ryg.j3o", new Vector3f(-4,-4,0), 180,270,0);
+        rootNode.attachChild(cube9);
      
+        
     }
     
     public void initLighting(){
@@ -50,6 +65,17 @@ public class Main extends SimpleApplication {
         rootNode.addLight(sun);
         
     }
+    
+    @Override
+public void simpleUpdate(float tpf) {
+
+    Quaternion rotationVelocityFPS=new Quaternion();
+    rotationVelocityFPS.slerp(Quaternion.IDENTITY, rotationVelocity, tpf);
+
+    rotation.multLocal(rotationVelocityFPS);
+
+    cube9.setLocalRotation(rotation);
+}
     
     public Spatial createSpatial(String modelPath, Vector3f translation, float rotX, float rotY, float rotZ ){
         
